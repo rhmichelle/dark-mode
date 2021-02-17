fe61a012410dc39151aab02b9a403a8bffdc9c51
michelleRH <dev@michellerh.com>
2021-02-16 19:58:16 -0500

    It works

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,3 +1,9 @@
-const useDarkMode => () {
-    
+import useLocalStorage from './useLocalStorage';
+
+
+const useDarkMode = () => {
+    const [darkModeIsOn, setDarkModeIsOn] = useLocalStorage('key');
+    return [darkModeIsOn, setDarkModeIsOn];
 }
+
+export default useDarkMode;
